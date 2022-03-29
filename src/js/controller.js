5d0f681fa912adf10f83cd1d685e74d8923ecee6
jamesserra <jamesserra4@gmail.com>
2022-03-29 14:27:36 -0400

    implement error messages

--- src/js/controller.js
+++ src/js/controller.js
@@ -27,7 +27,7 @@ const controlRecipes = async function() {
     //2) Rending Recipe
     recipeView.render(model.state.recipe)
   } catch (err) {
-    console.log(err);
+    recipeView.renderError()
   }
 };
 
