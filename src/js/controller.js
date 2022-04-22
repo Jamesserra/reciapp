fde673b0fc18ef82a17c20c0b91126f02fcc08a6
James Serra <Jamesserra4@gmail.com>
2022-04-21 22:05:36 -0400

    recipe add data

--- src/js/controller.js
+++ src/js/controller.js
@@ -4,6 +4,8 @@ import searchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
 import bookmarksView from './views/bookmarksView.js';
 import paginationView from './views/paginationView.js';
+import addRecipeView from './views/addRecipeView.js';
+
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
@@ -88,6 +90,10 @@ const controlBookmarks = function() {
   bookmarksView.render(model.state.bookmarks)
 }
 
+const controlAddRecipe = function(newRecipe) {
+  console.log(newRecipe);
+}
+
 const init = function() {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -95,6 +101,7 @@ const init = function() {
   recipeView.addHandlerAddBookmark(controlAddBookmark);
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
+  addRecipeView.addHandlerUpload(controlAddRecipe);
 };
 init();
 // window.addEventListener('hashchange', controlRecipes);
